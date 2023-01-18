15d99a8f399afe51b02284ad823b8daaa5831ced
Niraj Palecha <nirajftw@gmail.com>
2023-01-18 20:46:02 +0530

    add output iam_role_name

--- out.tf
+++ out.tf
@@ -2,3 +2,8 @@ output "iam_role_arn" {
   value       = aws_iam_role.oidc.arn
   description = "IAM role ARN"
 }
+
+output "iam_role_name" {
+  value       = aws_iam_role.oidc.name
+  description = "IAM role name"
+}
